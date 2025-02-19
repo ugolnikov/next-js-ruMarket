ab2f3035369c911e002241f50a2561e0f58bffaa
ugolnikov <hacker.2282005@mail.ru>
2025-02-19 10:35:51 +0500

    +vercel fix

--- src/app/(app)/dashboard/orders/page.js
+++ src/app/(app)/dashboard/orders/page.js
@@ -40,7 +40,7 @@ export default function OrdersPage() {
     if (loading) {
         return (
             <div className="flex justify-center items-center min-h-screen">
-                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900" />
             </div>
         )
     }
