df61eaa5d679047c5cbdbb92ecfcaf861cc944e8
ugolnikov <hacker.2282005@mail.ru>
2025-05-28 07:50:29 +0500

    +fix doable error

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -71,11 +71,11 @@ const Login = () => {
                                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Email"
                             />
-                            {errors.email && (
+                            {/* {errors.email && (
                                 <p className="text-red-500 text-sm mt-1">
                                     {errors.email.message}
                                 </p>
-                            )}
+                            )} */}
                         </div>
                         <div>
                             <input
